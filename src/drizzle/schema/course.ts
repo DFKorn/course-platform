e1655126d917f1f6db26354dd0a08b8aa2d548ed
DFKorn <d1man2192@mail.ru>
2025-02-25 22:15:10 +0300

    add schemas and realtions

--- src/drizzle/schema/course.ts
+++ src/drizzle/schema/course.ts
@@ -1,14 +1,16 @@
 import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { id, createdAt, updatedAt } from "../schemaHelpers";
+import { CourseProductTable } from "./courseProduct";
 
 export const CourseTable = pgTable("courses", {
-    id: uuid().primaryKey().defaultRandom(),
+    id,
     name: text().notNull(),
     description: text().notNull(),
-    createdAt: timestamp({withTimezone: true}).notNull().defaultNow(),
-    updatedAt: timestamp({withTimezone: true}).notNull().defaultNow().$onUpdate(() => new Date())
+    createdAt,
+    updatedAt,
 })
 
-export const CourseRelationships = relations(CourseTable, ({one, many}) => ({
-    
+export const CourseRelationships = relations(CourseTable, ({ many}) => ({
+    courseProducts: many(CourseProductTable),
 }))
