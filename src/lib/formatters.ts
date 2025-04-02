eaa6c470e42c41c8761db4a5732981b5ea3cdc43
DFKorn <d1man2192@mail.ru>
2025-04-02 20:57:15 +0300

    finish Admin Dashboard Page

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -29,3 +29,11 @@ export function formatPlural(
     return DATE_FORMATTER.format(date)
   }
   
+
+  export function formatNumber(
+    number: number,
+    options?: Intl.NumberFormatOptions
+  ){
+    const formatter = new Intl.NumberFormat(undefined, options)
+    return formatter.format(number)
+  }
