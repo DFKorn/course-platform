357302bfc5435eb11d1c1f2f077a3da41cd43596
DFKorn <d1man2192@mail.ru>
2025-04-09 21:50:03 +0300

    add Suspense boundary to Sign-in/up pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,21 @@
-'use client'
+// 'use client'
 
 import { SignIn } from '@clerk/nextjs'
+import { Suspense } from 'react'
+
+// export default function Page() {
+//   return <SignIn fallback={<div>Loading...</div>}/>
+// }
+
+
+function FallbackSkeleton() {
+  return <div>Loading...</div>
+}
 
 export default function Page() {
-  return <SignIn fallback={<div>Loading...</div>}/>
+  return (
+    <Suspense fallback={<FallbackSkeleton />}>
+      <SignIn fallback={<FallbackSkeleton />}/>
+    </Suspense>
+  )
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,22 @@
-'use client'
+// 'use client'
 
 import { SignUp } from '@clerk/nextjs'
+import { Suspense } from 'react'
+
+// export default function Page() {
+//   return <SignUp fallback={<div>Loading...</div>}/>
+// }
+
+
+function FallbackSkeleton() {
+  return <div>Loading...</div>
+}
+
 
 export default function Page() {
-  return <SignUp fallback={<div>Loading...</div>}/>
+  return (
+    <Suspense fallback={<FallbackSkeleton/>}> 
+      <SignUp fallback={<FallbackSkeleton/>}/>
+    </Suspense>
+  )
 }
