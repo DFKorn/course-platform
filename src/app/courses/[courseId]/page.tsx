99cd820291004d3704d7c817f454d59106d501f1
DFKorn <d1man2192@mail.ru>
2025-05-07 21:39:33 +0300

    add <Suspense>

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -5,12 +5,27 @@ import { getCourseIdTag } from "@/features/courses/db/cache/courses"
 import { eq } from "drizzle-orm"
 import { cacheTag } from "next/dist/server/use-cache/cache-tag"
 import { notFound } from "next/navigation"
+import { SkeletonText } from "@/components/Skeleton"
+import { Suspense } from "react"
 
 export default async function CoursePage({
   params,
 }: {
   params: Promise<{ courseId: string }>
 }) {
+  return(
+    <Suspense fallback={<CourseSkeleton/>}>
+      <SuspendedComponent params={params}/>
+    </Suspense>
+  )
+}
+
+
+async function SuspendedComponent({
+  params
+}:{
+  params:Promise<{courseId: string}>
+}){
   const { courseId } = await params
   const course = await getCourse(courseId)
 
@@ -20,6 +35,15 @@ export default async function CoursePage({
     <div className="my-6 container">
       <PageHeader className="mb-2" title={course.name} />
       <p className="text-muted-foreground">{course.description}</p>
+    </div>
+  )
+}
+
+function CourseSkeleton(){
+  return(
+    <div className="my-6 container">
+      <SkeletonText className="w-42 mb-2" size="lg" />
+      <SkeletonText className="w-3/4"  />
     </div>
   )
 }
